6194fdae75b84a523b6024e1e923051df24f0c34
jeogwon.yun <jeongwon.yun@howoocast.com>
2024-04-19 07:19:56 +0900

    4/19 모닝, css module을 선택한 이유까지

---
+++ z-com/src/app/(afterLogin)/home/layout.tsx
@@ -0,0 +1,6 @@
+export default async function HomeLayout({children}) {
+    return (
+        <div>홈 레이아웃
+            {children}</div>
+    )
+}
---
+++ z-com/src/app/(afterLogin)/layout.tsx
@@ -0,0 +1,6 @@
+export default async function AfterLoginLayout({children}) {
+    return (
+        <div>에프터 로그인 레이아웃
+            {children}</div>
+    )
+}
